e3dafedf332e3b51998cb98b1fac2eb80cd0b8ad
Aniket <aniketroy9135@gmail.com>
2024-04-23 10:14:49 +0530

    work on component and pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+  const authStatus = useSelector((state) => state.status);
 
-export default Header
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Singup",
+      slug: "/singup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+
+
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    onClick={() => navigate(item.slug)}
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {
+              authStatus && (
+                <li>
+                  <LogoutBtn/>
+                </li>
+              )
+            }
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Logo from "./Logo";
+import LogoutBtn from './Header/LogoutBtn'
+import Container from "./container/Container";
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    LogoutBtn,
+    Container,
 }
