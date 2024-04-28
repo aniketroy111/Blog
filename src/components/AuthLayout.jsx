d86f71aac52d3ad1be041382daf91429f7eb48ab
Aniket <aniketroy9135@gmail.com>
2024-04-28 12:16:01 +0530

    Protect navigation

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,34 @@
+import React,{useEffect,useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+const AuthLayout = ({children,authentication=true}) => {
+    const [loader,setLoader] = useState(true);
+    const navigate = useNavigate();
+    const authStatus = useSelector(state=>state.auth.status);
+
+
+    useEffect(()=>{
+        if(authStatus){
+            navigate("/");
+        }
+        else if(authStatus===false){
+            navigate("/login");
+        }
+        // const authValue = authStatus===true? true:false;
+        // if(authentication && authStatus!==authentication){
+        //     navigate('/login');
+        // }
+        // else if(!authentication && authStatus!==authentication){
+        //     navigate('/');
+        // }
+
+        setLoader(false);
+
+    },[authStatus,navigate,authentication])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
+
+export default AuthLayout
